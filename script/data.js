09505451cb50485a1475a5a5f193cd7db9c959c5
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-06 21:04:51 +0100

    update design

---
+++ script/data.js
@@ -0,0 +1,7 @@
+const DATA = {
+    "first_name": "Abdellatif",
+    "last_name": "Laghjaj",
+    "age": "20",
+    "city": "Taroudant",
+    "country": "Morocco",
+    "email": "abdelatiflaghjaj@gmail.com",}
